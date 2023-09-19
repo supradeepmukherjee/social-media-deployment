8277d3eddc003aeb4cf4c40afd3881856a92d353
Supradeep <supradeep2004@gmail.com>
2023-09-19 19:38:36 +0530

    fixing file path

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use('/api/v1', post)
 app.use('/api/v1', user)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')))
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "/frontend/build/index.html")))
 }
