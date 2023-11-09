9d57fed1be99592809b34cb1fec358a6f536f514
Supradeep <supradeep2004@gmail.com>
2023-11-09 20:54:28 +0530

    Added Tooltip Component in Header

--- frontend/src/components/Header/Navbar.js
+++ frontend/src/components/Header/Navbar.js
@@ -2,23 +2,32 @@ import { Link } from 'react-router-dom'
 import './Header.css'
 import { Home, HomeOutlined, Add, AddOutlined, Search, SearchOutlined, AccountCircle, AccountCircleOutlined } from '@mui/icons-material'
 import { useState } from 'react'
+import { Tooltip } from '@mui/material'
 
 const Navbar = () => {
     const [tab, setTab] = useState(window.location.pathname)
     return (
         <div className='header'>
-            <Link to={'/'} onClick={() => setTab('/')}>
-                {tab === '/' ? <Home style={{ color: 'black' }} /> : <HomeOutlined />}
-            </Link>
-            <Link to={'/newpost'} onClick={() => setTab('/newpost')}>
-                {tab === '/newpost' ? <Add style={{ color: 'black' }} /> : <AddOutlined />}
-            </Link>
-            <Link to={'/search'} onClick={() => setTab('/search')}>
-                {tab === '/search' ? <Search style={{ color: 'black' }} /> : <SearchOutlined />}
-            </Link>
-            <Link to={'/account'} onClick={() => setTab('/account')}>
-                {tab === '/account' ? <AccountCircle style={{ color: 'black' }} /> : <AccountCircleOutlined />}
-            </Link>
+            <Tooltip title="Home" arrow>
+                <Link to={'/'} onClick={() => setTab('/')}>
+                    {tab === '/' ? <Home style={{ color: 'black' }} /> : <HomeOutlined />}
+                </Link>
+            </Tooltip>
+            <Tooltip title="Create New Post" arrow>
+                <Link to={'/newpost'} onClick={() => setTab('/newpost')}>
+                    {tab === '/newpost' ? <Add style={{ color: 'black' }} /> : <AddOutlined />}
+                </Link>
+            </Tooltip>
+            <Tooltip title="Search" arrow>
+                <Link to={'/search'} onClick={() => setTab('/search')}>
+                    {tab === '/search' ? <Search style={{ color: 'black' }} /> : <SearchOutlined />}
+                </Link>
+            </Tooltip>
+            <Tooltip title="My Profile" arrow>
+                <Link to={'/account'} onClick={() => setTab('/account')}>
+                    {tab === '/account' ? <AccountCircle style={{ color: 'black' }} /> : <AccountCircleOutlined />}
+                </Link>
+            </Tooltip>
         </div>
     )
 }
