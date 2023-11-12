d46ea2453f11a1526fceb178cf7e1662d88076ab
Supradeep <supradeep2004@gmail.com>
2023-11-12 12:11:50 +0530

    Optimised DB Connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose')
 
 exports.connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_URI,
-        {
-            useNewUrlParser: true,
-            // useCreateIndex: true,
-            // useFindAndModify: false,
-            useUnifiedTopology: true
-        }
-    )
+    mongoose.connect(process.env.MONGO_URI)
         .then(con => console.log(`db ${con.connection.host}`))
         .catch(err => console.log(err))
 }
