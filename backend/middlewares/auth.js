f95fafbd136113e577b28b174e50c220414eb7d1
Supradeep <supradeep2004@gmail.com>
2023-11-21 15:30:26 +0530

    Fixed a minor bug

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 exports.isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies
-    if (!token) return res.status(401).json({ msg: 'Please login first' })
+    if (!token) return
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     req.user = await User.findById(decoded.id)
     next()
