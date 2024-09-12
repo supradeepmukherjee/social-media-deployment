4212beddab67be26fed0cf465e06bb7e657e80fd
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-09-13 00:10:18 +0530

    .

--- frontend/src/components/Loader/Loader.jsx
+++ frontend/src/components/Loader/Loader.jsx
@@ -1,9 +1,13 @@
+import { Typography } from '@mui/material'
 import './Loader.css'
 
 const Loader = () => {
   return (
     <div className='page'>
       <div className="circle"></div>
+      <Typography>
+        Please wait. The site is deployed using a free instance on render.com which takes 50 seconds to spin up
+      </Typography>
     </div>
   )
 }
